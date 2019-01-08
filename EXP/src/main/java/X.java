412f779f9d5c18a5fe06ebb62302fef9290bde97
nuno837 <nunofilmaia@yahoo.com.br>
2019-01-08 02:53:07 +0000

    Update codificação: a ler varios ficheiros

--- EXP/src/main/java/X.java
+++ EXP/src/main/java/X.java
@@ -14,60 +14,64 @@ public class X {
 
     public static void main(String[] args) throws Exception {
 
-        File file = new File("C:\\Users\\vieir\\Documents\\GitHub\\ESII--TP2\\EXP\\file.txt");
+        File file = new File("C:\\Users\\nunof\\Desktop\\Universidade\\PAW\\ESII--TP2.1\\EXP");
 
-        BufferedReader br = new BufferedReader(new FileReader(file));
+        FilenameFilter textFilter = new FilenameFilter() {
+            public boolean accept(File dir, String name) {
+                return name.toLowerCase().endsWith(".txt");
+            }
+        };
 
-        String st;
-        while ((st = br.readLine()) != null) {
-            //System.out.println(st);
-
-           // System.out.println(new X().remove(st));
-
-        }
+        File[] files = file.listFiles(textFilter);
 
 
         int count = 0;
         ArrayList<Palavra> palavra = new ArrayList();
 
-        try (LineNumberReader r = new LineNumberReader(new FileReader(file))) {
-            String line;
+        Matriz matriz = new Matriz(0, "", 0);
 
-            while ((line = r.readLine()) != null) {
-                String linha = new X().remove(line).toLowerCase();
+        for (int k = 0; k < files.length; k++) {
+            System.out.println(files[k].getCanonicalPath());
 
-                for (String element : linha.split(" ")) {
-                   // System.out.println(element);
-                if(!element.equals("")){
-                    if (palavra.size() == 0) {
-                        Palavra palavra3 = new Palavra(element, 1);
-                        palavra.add(palavra3);
-                    } else {
-                        for (int i = 0; i < palavra.size(); i++) {
-                            if (element.equals(palavra.get(i).getDescricao())) {
-                                palavra.get(i).setCount(palavra.get(i).getCount() + 1);
-                                count++;
+            try (LineNumberReader r = new LineNumberReader(new FileReader(files[k]))) {
+                String line;
+
+                while ((line = r.readLine()) != null) {
+                    String linha = new X().remove(line).toLowerCase();
+
+                    for (String element : linha.split(" ")) {
+                        // System.out.println(element);
+                        if (!element.equals("")) {
+                            if (palavra.size() == 0) {
+                                Palavra palavra3 = new Palavra(element, 1);
+                                palavra.add(palavra3);
+                            } else {
+                                for (int i = 0; i < palavra.size(); i++) {
+                                    if (element.equals(palavra.get(i).getDescricao())) {
+                                        palavra.get(i).setCount(palavra.get(i).getCount() + 1);
+                                        count++;
+                                    }
+                                }
+                                if (count == 0) {
+                                    Palavra palavra1 = new Palavra(element, 1);
+                                    palavra.add(palavra1);
+                                } else {
+                                    count = 0;
+                                }
+
                             }
-                        }
-                        if(count==0) {
-                            Palavra palavra1 = new Palavra(element, 1);
-                            palavra.add(palavra1);
-                        }else{
-                            count=0;
-                        }
 
+                        }
                     }
-
-                }}
+                }
             }
+
         }
+
         for (int i = 0; i < palavra.size(); i++) {
-            System.out.println(palavra.get(i).getDescricao());
-            System.out.println(palavra.get(i).getCount());
+            System.out.println(palavra.get(i).getDescricao()+"-> "+palavra.get(i).getCount());
         }
-
     }
-
     public String remove(String src) {
         char[] srcArr = src.toCharArray();
         StringBuilder sb = new StringBuilder();
