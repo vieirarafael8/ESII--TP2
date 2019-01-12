ce079caadcb5ec44cde60ed5efca9c7b021a034e
vieirarafael8 <32908327+vieirarafael8@users.noreply.github.com>
2019-01-12 04:11:33 +0000

    update tabelas e testes

--- EXP/src/test/java/TratamentoTest.java
+++ EXP/src/test/java/TratamentoTest.java
@@ -6,11 +6,14 @@ import static org.junit.Assert.*;
 public class TratamentoTest {
 
     Tratamento t = new Tratamento();
+    char[] delete = {'\n', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '?', '.', ';', ',', ':', '!', '-', '(', ')', '_', '/', '*', '[', ']'};
+
 
     @Test
     public void isFound() {
 
-        
+
+
     }
 
     @Test
